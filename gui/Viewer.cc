5b691c02ec229b1754753f12060726fe8ad761c2
Fabian Schenk <fabian.schenk@icg.tugraz.at>
2017-12-29 11:32:42 +0100

    Reduced sleep time of viewer to make the visualization look smoother

--- gui/Viewer.cc
+++ gui/Viewer.cc
@@ -156,7 +156,7 @@ void Viewer::Run()
         }
         if(CheckFinish())
             break;
-        usleep(100000);
+        usleep(1000);
     }
     I3D_LOG(i3d::debug) << "Stop";
     if (mSaveModel)
